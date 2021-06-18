db572196c8e5892cd7396b0a67fbde06cc2e7e87
Rafe <rafenasir@gmail.com>
2021-06-18 13:39:49 +0200

    small changhes

--- template.js
+++ template.js
@@ -4,7 +4,7 @@ document.write(`\
     <div class="container">\
     <img src="./media/blue_background.png" alt="background" width="100%" height="100vh">
         <div class="col pl-0 pr-0">\
-            <ul class="nav justify-content-end";>\
+            <ul class=" mb-0 pt-2 nav justify-content-end";>\
                 <li class="nav-item"><a class="nav-link active text-white " href="index.html">Home</a></li>\
                 <li class="nav-item"><a class="nav-link active text-white" href="project.html">Management System</a></li>\
                 <li class="nav-item"><a class="nav-link active text-white" href="contact.html">Contact</a></li>\
@@ -15,39 +15,39 @@ document.write(`\
 </template>\
 `);
 
-
 function showNavTemplate() {
-    sample1 = document.getElementById('nav-template').content;
-    templateNode = document.importNode(sample1, true);
-    document.getElementById("template-div").appendChild(templateNode);
+  sample1 = document.getElementById("nav-template").content;
+  templateNode = document.importNode(sample1, true);
+  document.getElementById("template-div").appendChild(templateNode);
 }
 
-document.addEventListener('DOMContentLoaded', function(event) {
-    showNavTemplate();
-    customiseToastr();
-
-})
+document.addEventListener("DOMContentLoaded", function (event) {
+  showNavTemplate();
+  customiseToastr();
+});
 
 // bg-primary
 
-{ /* <img src="./media/blue_background.png" alt="background" width="100%" height="100vh">\ */ }
+{
+  /* <img src="./media/blue_background.png" alt="background" width="100%" height="100vh">\ */
+}
 
 function customiseToastr() {
-    toastr.options = {
-        "closeButton": true,
-        "debug": false,
-        "newestOnTop": false,
-        "progressBar": false,
-        "positionClass": "toast-top-right",
-        "preventDuplicates": true,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-    }
+  toastr.options = {
+    closeButton: true,
+    debug: false,
+    newestOnTop: false,
+    progressBar: false,
+    positionClass: "toast-top-right",
+    preventDuplicates: true,
+    onclick: null,
+    showDuration: "300",
+    hideDuration: "1000",
+    timeOut: "5000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut",
+  };
 }
